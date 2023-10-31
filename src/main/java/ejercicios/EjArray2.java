b4320dec3658cd1712ccf1ab72ce33b2f3c144f0
tgonati327 <tgonati327@g.educaand.es>
2023-10-31 12:32:07 +0100

    v1

--- src/main/java/ejercicios/EjArray2.java
+++ src/main/java/ejercicios/EjArray2.java
@@ -4,6 +4,9 @@
  */
 package ejercicios;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  *
  * @author tomas
@@ -13,10 +16,110 @@ De cada animal vamos a guardar su peso, que vamos a pedir por teclado.
 Una vez sepamos los pesos de todos los animales, queremos saber la media de todos los pesos, 
 cuantos animales hay por debajo de la media y cuantos por encima
 Si el usuario pone un peso negativo, se considerará positivo. */
-
 public class EjArray2 {
 
     public static void main(String[] args) {
-        
+        int numAnimales = solicitarDatoInt();
+        //String[] arrayNombreAnimales = new String[numAnimales];
+        double[] arrayPesoAnimales = new double[numAnimales];
+        arrayPesoAnimales = rellenarArrayPesos(arrayPesoAnimales);
+        mostrarArray(arrayPesoAnimales);
+        System.out.println("********************PESO ANIMALES********************");
+        mostrarDatosPesos(arrayPesoAnimales);
+//
+//
+///
+//
+//
+
+//
+//
+//
+        //
     }
+
+    // Solicitar Nº animales.
+    public static int solicitarDatoInt() {
+        Scanner teclado = new Scanner(System.in);
+        boolean seguir = true;
+        int dato = 0;
+        do {
+            System.out.println("Introduce el número de animales a evaluar");
+            try {
+                dato = teclado.nextInt();
+                teclado.nextLine();
+                seguir = false;
+            } catch (InputMismatchException ime) {
+                System.out.println("ERROR.No has introduciod un numero\n");
+                teclado.nextLine();
+            }
+        } while (seguir);
+
+        return dato;
+    }
+
+    // Solicitar peso de cada animal.
+    public static double solicitarDatoDouble() {
+
+        Scanner teclado = new Scanner(System.in);
+        boolean seguir = true;
+        double dato = 0;
+        do {
+            System.out.println("Introduce el peso: (5,5)");
+            try {
+                dato = teclado.nextDouble();
+                teclado.nextLine();
+                seguir = false;
+            } catch (InputMismatchException ime) {
+                System.out.println("ERROR. Dato no válido\n");
+                teclado.nextLine();
+            }
+        } while (seguir);
+        return dato;
+    }
+
+    // Rellenar array con peso de animales.
+    public static double[] rellenarArrayPesos(double[] array) {
+        Scanner teclado = new Scanner(System.in);
+        double peso;
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("Animal " + (i + 1) + "/" + array.length);
+            peso = solicitarDatoDouble();
+            array[i] = peso;
+            System.out.println("\n");
+
+        }
+        return array;
+    }
+
+    public static String[] rellenarArrayNombres(String[] array) {
+        Scanner teclado = new Scanner(System.in);
+        String nombre = "";
+        for (int i = 0; i < array.length; i++) {
+
+        }
+        return array;
+    }
+
+    //Mostar array rellenado.
+    public static void mostrarArray(double[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+
+    //Calcular peso Animales
+    public static void mostrarDatosPesos(double[] array) {
+
+        int numAnimalesTotales = array.length;
+        double pesoTotal = 0;
+        for (int i = 0; i < array.length; i++) {
+            pesoTotal = array[i];
+            pesoTotal += pesoTotal;
+        }
+
+        double media = pesoTotal / numAnimalesTotales;
+        System.out.println("Media de pesos: " + media + "Kg");
+    }
+
 }
