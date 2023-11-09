ea69874f992bb69f39bb8932adb8d8afc4fddc27
tgonati327 <tgonati327@g.educaand.es>
2023-11-09 15:35:11 +0100

    casa

---
+++ src/main/java/vehiculos/MisVehiculos.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vehiculos;
+
+/**
+ *
+ * @author tomas
+ */
+public class MisVehiculos {
+    public static void main(String[] args) {
+        // Creo el objeto
+        Vehiculo miCacharro = new Vehiculo("AAAA", "Seat", "LEon", "Negro", 100.0, true);
+        
+        // Muestro sus valores uno a uno
+        System.out.println(miCacharro.matricula);
+        System.out.println(miCacharro.marca);
+        System.out.println(miCacharro.modelo);
+        System.out.println(miCacharro.color);
+        System.out.println(miCacharro.tarifa);
+        System.out.println(miCacharro.disponible);
+        System.out.println("------------------------------------");
+        miCacharro.setMatricula("BBBB");
+        System.out.println(miCacharro.matricula);
+        
+        
+        
+        
+    }
+    
+    
+    
+}
---
+++ src/main/java/vehiculos/Vehiculo.java
@@ -0,0 +1,84 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vehiculos;
+
+/**
+ *
+ * @author tomas
+ */
+public class Vehiculo {
+
+    String matricula;
+    String marca;
+    String modelo;
+    String color;
+    double tarifa;
+    boolean disponible;
+
+    // Constructor
+    public Vehiculo(String matricula, String marca, String modelo, String color, double tarifa, boolean disponible) {
+        this.matricula = matricula;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.color = color;
+        this.tarifa = tarifa;
+        this.disponible = disponible;
+    }
+
+    // Getters & Setters
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getTarifa() {
+        return tarifa;
+    }
+
+    public void setTarifa(double tarifa) {
+        this.tarifa = tarifa;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+
+    // Método
+    public void aplicarDescuentoTarifa(double descuento) {
+        this.tarifa -= descuento;
+    }
+
+}
